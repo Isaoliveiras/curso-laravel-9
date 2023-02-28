2b8ae228b182c31bd51f6a87ab00a741c67f58dc
Isabella <isabella_oliveiras@outlook.com>
2023-02-28 10:00:55 -0300

    First Commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('users.index');
+    }
+
+    public function show($id)
+    {
+        dd ('users.show', $id);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\{
+    UserController
+};
 use Illuminate\Support\Facades\Route;
 
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
 
 Route::get('/', function () {
     return view('welcome');
